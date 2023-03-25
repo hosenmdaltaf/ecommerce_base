3381c1da3db056795207df2b47631a243626e915
hosenmdaltaf <altafhmeazee1999@outlook.com>
2023-03-25 17:00:10 +0600

    admin pannel setup complate

--- ecommerce_base/ecommerce/apps/custom_admin/urls.py
+++ ecommerce_base/ecommerce/apps/custom_admin/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from . import views
 
-app_name = 'admin'
+app_name = 'custom_admin'
 
 urlpatterns = [
     path('', views.home, name='admin_home'),
+    path('product_add/', views.product_add, name='product_add'), 
+    path('product_list/', views.product_list, name='product_list'), 
+
+    path('order_list/', views.order_list, name='order_list'), 
 ]
--- ecommerce_base/ecommerce/apps/custom_admin/views.py
+++ ecommerce_base/ecommerce/apps/custom_admin/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'custom_admin/homepage.html')
+
+def product_add(request):
+    return render(request,'custom_admin/add_product.html')
+
+def product_list(request):
+    return render(request,'custom_admin/product_list.html')
+
+def order_list(request):
+    return render(request,'custom_admin/order_list.html')
